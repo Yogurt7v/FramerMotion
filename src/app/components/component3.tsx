fdcf352812934a18b165c9cf9e23bccce6f17a08
Yogurt7v <egorov-2k@yandex.ru>
2025-05-05 20:22:10 +0300

    add: component 3 - шляпа

---
+++ src/app/components/component3.tsx
@@ -0,0 +1,5 @@
+export const Component3 = () => {
+  return (
+    <div className="min-h-screen bg-neutral-900 flex justify-center items-center"></div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Component2 } from './components/component2';
+// import { Component2 } from './components/component2';
 // import { Content } from './components/content';
+import { Component3 } from './components/component3';
+
 export default function Home() {
   return (
     <>
-      {/* <Content /> */}
-      <Component2 />
+      {/* <Content /> кнопка */}
+      {/* <Component2 /> карточка*/}
+      <Component3 />
     </>
   );
 }
